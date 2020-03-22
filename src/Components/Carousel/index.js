1fbff13543ef8c5c07762cdb63abde0c51a09732
Sourav Jana <sourav.jana@juntrax.com>
2020-03-22 14:51:26 +0530

    fix: 🐛 SCarousel

--- src/Components/Carousel/index.js
+++ src/Components/Carousel/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { Carousel } from 'antd'
 
 export const SCarousel = ({ children, ...props }) => (
-    <Carousel autoplay {...props}>
+    <Carousel {...props}>
         {children}
     </Carousel>
 )
