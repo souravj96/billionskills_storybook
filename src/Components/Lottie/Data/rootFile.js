e2e816ea73b25ffc5908ad17cca7404ed703658e
Sourav Jana <sourav.jana@juntrax.com>
2020-04-12 12:14:03 +0530

    feat: 🎸 new Lottie Icons added

--- src/Components/Lottie/Data/rootFile.js
+++ src/Components/Lottie/Data/rootFile.js
@@ -1,5 +1,11 @@
 import * as userDesktop from './user-desktop.json'
+import * as signIn from './signin.json'
+import * as signUp from './signup.json'
+import * as welcome from './welcome.json'
 
 export default {
     userDesktop: userDesktop,
+    signIn: signIn,
+    signUp: signUp,
+    welcome: welcome
 }
--- src/Components/Lottie/index.stories.js
+++ src/Components/Lottie/index.stories.js
@@ -6,4 +6,7 @@ export default {
     title: 'Lottie',
 };
 
-export const UserDesktop = () => <SLottie type="userDesktop" height={200} width={200}/>;
+export const UserDesktop = () => <SLottie type="userDesktop" height={200} width={200} />;
+export const SignIn = () => <SLottie type="signIn" height={200} width={200} />;
+export const SignUp = () => <SLottie type="signUp" height={200} width={200} />;
+export const Welcome = () => <SLottie type="welcome" height={200} width={200} />;
