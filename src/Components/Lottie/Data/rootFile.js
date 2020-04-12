cb264aff025c7aab7861339629465b3f74aba1e1
Sourav Jana <sourav.jana@juntrax.com>
2020-04-12 13:01:14 +0530

    feat: 🎸 more lottie files

--- src/Components/Lottie/Data/rootFile.js
+++ src/Components/Lottie/Data/rootFile.js
@@ -2,10 +2,24 @@ import * as userDesktop from './user-desktop.json'
 import * as signIn from './signin.json'
 import * as signUp from './signup.json'
 import * as welcome from './welcome.json'
+import * as noDataBox from './nodatabox.json'
+import * as noDataFile from './nodatafile.json'
+import * as noInternet from './nointernet.json'
+import * as noNotification from './nonotification.json'
+import * as noSearch from './nosearch.json'
+import * as error404 from './error404.json';
+import * as error500 from './error500.json'
 
 export default {
     userDesktop: userDesktop,
     signIn: signIn,
     signUp: signUp,
-    welcome: welcome
+    welcome: welcome,
+    noDataBox: noDataBox,
+    noDataFile: noDataFile,
+    noInternet: noInternet,
+    noNotification: noNotification,
+    noSearch: noSearch,
+    error404: error404,
+    error500: error500
 }
--- src/Components/Lottie/index.stories.js
+++ src/Components/Lottie/index.stories.js
@@ -10,3 +10,10 @@ export const UserDesktop = () => <SLottie type="userDesktop" height={200} width=
 export const SignIn = () => <SLottie type="signIn" height={200} width={200} />;
 export const SignUp = () => <SLottie type="signUp" height={200} width={200} />;
 export const Welcome = () => <SLottie type="welcome" height={200} width={200} />;
+export const NoDataBox = () => <SLottie type="noDataBox" height={200} width={200} />;
+export const NoDataFile = () => <SLottie type="noDataFile" height={200} width={200} />;
+export const NoInternet = () => <SLottie type="noInternet" height={200} width={200} />;
+export const NoNotification = () => <SLottie type="noNotification" height={200} width={200} />;
+export const NoSearc = () => <SLottie type="noSearch" height={200} width={200} />;
+export const Error404 = () => <SLottie type="error404" height={200} width={200} />;
+export const Error500 = () => <SLottie type="error500" height={200} width={200} />;
